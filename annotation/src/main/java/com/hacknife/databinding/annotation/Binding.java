3f5e63e999482e8c8682b03dad669434f76fa1c8
hacknife <hacknife@outlook.com>
2020-10-20 16:33:32 +0800

    clear

--- annotation/src/main/java/com/hacknife/databinding/annotation/Binding.java
+++
@@ -1,9 +0,0 @@
-package com.hacknife.databinding.annotation;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-public @interface Binding {
-
-}
